38c7673d0b035ead9e1669759294e74625e7155c
Gijsdeman <github@deman.dev>
2024-11-03 18:14:05 +0100

    feat(common): extend common config

--- eslint.common.mjs
+++ eslint.common.mjs
@@ -1,42 +1,51 @@
 import eslint from '@eslint/js';
 import tsEslint from 'typescript-eslint';
 import importPlugin from 'eslint-plugin-import';
-import prettierConfig from 'eslint-config-prettier';
+
+import {includeIgnoreFile} from "@eslint/compat";
+import path from "node:path";
+import {fileURLToPath} from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default [
-    eslint.configs.recommended,
-    importPlugin.flatConfigs.recommended,
-    importPlugin.flatConfigs.typescript,
-    {
-        files: ['src/**/*.{js,ts,jsx,tsx,vue}'],
-        languageOptions: {
-            ecmaVersion: 'latest'
-        },
-        settings: {
-            'import/resolver': {
-                typescript: {
-                    project: './tsconfig.app.json'
-                },
-                node: {
-                    project: './tsconfig.node.json'
-                }
-            }
-        },
-        rules: {
-            'import/order': ['warn'],
-            'import/first': ['warn'],
-            // General JS/TS rules ported over
-            'max-len': ['warn', { code: 120 }],
-            'semi': ['error', 'always'],
-            'object-curly-spacing': ['error', 'always'],
-        }
+  includeIgnoreFile(gitignorePath),
+  eslint.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
+  {
+    files: ['src/**/*.{js,ts,jsx,tsx,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest'
     },
-    ...tsEslint.configs.recommended,
-    {
-        files: ['src/**/*.{ts,tsx,vue}'],
-        languageOptions: {
-            parser: tsEslint.parser
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.app.json'
+        },
+        node: {
+          project: './tsconfig.node.json'
         }
+      }
     },
-    prettierConfig,
+    rules: {
+      'import/order': ['warn'],
+      'import/first': ['warn'],
+      // General JS/TS rules ported over
+      'max-len': ['warn', {code: 120}],
+      'semi': ['error', 'always'],
+      'object-curly-spacing': ['error', 'always'],
+      "linebreak-style": ["error", "unix"],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    }
+  },
+  ...tsEslint.configs.recommended,
+  {
+    files: ['src/**/*.{ts,tsx,vue}'],
+    languageOptions: {
+      parser: tsEslint.parser
+    }
+  },
 ];
