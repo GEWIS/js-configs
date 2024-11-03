604d421b2ea7ec82ff3e5d90a47e6d662f925927
Gijsdeman <github@deman.dev>
2024-11-03 18:14:57 +0100

    feat(react): add react config

--- eslint.common.mjs
+++ eslint.common.mjs
@@ -4,14 +4,9 @@ import importPlugin from 'eslint-plugin-import';
 
 import {includeIgnoreFile} from "@eslint/compat";
 import path from "node:path";
-import {fileURLToPath} from "node:url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default [
-  includeIgnoreFile(gitignorePath),
+  includeIgnoreFile(path.resolve(".gitignore")),
   eslint.configs.recommended,
   importPlugin.flatConfigs.recommended,
   importPlugin.flatConfigs.typescript,
