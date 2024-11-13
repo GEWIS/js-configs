874d14538d708b3885ef3909915ea17167205633
Gijsdeman <github@deman.dev>
2024-11-13 13:54:49 +0100

    refactor(configs): make configs more structured
    Use a more "default" set-up, where every plugin is chained separately
    for clearer control.
    
    Also fixes persisting issues with react config, and adds stricter type
    checking for TS.

--- eslint.react.mjs
+++ eslint.react.mjs
@@ -2,22 +2,54 @@ import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactA11y from 'eslint-plugin-jsx-a11y'
 
+import {fixupPluginRules} from '@eslint/compat';
+
 export default [
-  reactPlugin.configs.flat.recommended,
-  reactPlugin.configs.flat['jsx-runtime'],
-  reactA11y.flatConfigs.recommended,
+  // React plugin
   {
-    files: ['src/**/*.{jsx,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        },
+        jsxPragma: null,
+      }
+    },
     plugins: {
-      'react-hooks': reactHooks
+      "react": reactPlugin,
     },
     settings: {
       react: {
-        version: 'detect'
-      }
+        version: 'detect',
+      },
+    },
+  },
+  // Default rules
+  {
+    files: ['src/**/*.tsx'],
+    rules: {
+      ...reactPlugin.configs.flat.recommended.rules,
+      ...reactPlugin.configs.flat['jsx-runtime'].rules,
+    }
+  },
+  // A11y
+  {
+    files: ['src/**/*.tsx'],
+    plugins: {
+      'jsx-a11y': reactA11y
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      ...reactA11y.flatConfigs.recommended.rules
+    }
+  },
+  // React hooks
+  {
+    files: ['src/**/*.tsx'],
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules
     },
   },
 ];
